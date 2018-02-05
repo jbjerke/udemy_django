eaee669a91eb53f0c48a86a5f3724b448cb0be5e
Jordan Bjerken <jbjerken@gmail.com>
2018-02-05 08:02:06 -0500

    bootstrap done

---
+++ javascript/part_one/connecting_html.js
@@ -0,0 +1,5 @@
+alert("Welcome to your bank");
+var deposit = prompt("How much would you like to deposit?");
+alert("You've deposited: " + deposit);
+// console requires us to inspect the page
+console.log("You are a cool person!");
---
+++ javascript/part_one/mypart3.js
@@ -0,0 +1,4 @@
+var pnds = prompt("Please enter a weight in pounds (lbs.):");
+var kgm = pnds * 0.454;
+alert(pnds + "lbs is " + kgm + "kg");
+console.log("Conversion Complete");
